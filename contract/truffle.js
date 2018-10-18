332d8c60f36732eba07207996001ba6599df8ed5
Luca Ban - Black Birdy <mesqueeb@gmail.com>
2018-10-18 13:34:05 +0900

    Bug fix related to ropsten provider 🎏

--- contract/truffle.js
+++ contract/truffle.js
@@ -2,7 +2,9 @@ const mnemonic = process.env.MNEMONIC_KEY
 const HDWalletProvider = require('truffle-hdwallet-provider')
 // const mnemonic = "recipe vintage differ tobacco venture federal inquiry cross pig bean adapt seven"
 
-var ropsten = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/', process.env.ACCOUNT)
+var ropsten = function () {
+  return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/', process.env.ACCOUNT)
+}
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -22,7 +24,7 @@ module.exports = {
       gasPrice: 10000000000
     },
     ropsten: {
-      provider: ropsten,
+      provider: ropsten(),
       network_id: 3, // Match ropsten network id
       gas: 4580036,
       gasPrice: 3000000000
