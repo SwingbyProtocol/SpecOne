d48a32231e1bb7ec2a0578a6e0aed8d33b63bcac
Luca Ban - Black Birdy <mesqueeb@gmail.com>
2018-10-19 16:12:28 +0900

    add passable parameters 🐿

--- contract/txtest/ethtx/submitOrder.js
+++ contract/txtest/ethtx/submitOrder.js
@@ -5,40 +5,37 @@ const Swingby = artifacts.require('./Swingby.sol')
 const Token = artifacts.require('./Token.sol')
 
 const address = getAddress()
-const arg1 = Number(process.argv[4])
+const argBtct = Number(process.argv[4])
+const argEth = Number(process.argv[5])
+const argSr = process.argv[6]
 
 module.exports = async function (callback) {
-    if (!arg1) return callback('Requires the amount as first argument')
+    if (!argBtct) return callback('Requires the amount as first argument')
+    if (!argEth) return callback('Requires collateral')
+    if (!argSr) return callback('Requires sR')
     const sw = await Swingby.deployed()
 
-    const balance = await sw.balanceOfETH(address)
-    console.log(balance.toNumber() / 1e18)
-
-    const _amountOfSat = arg1 * 1e18
-    const _amountOfWei = 1 * 1e18
+    const _amountOfSat = argBtct * 1e18
+    const _amountOfWei = argEth * 1e18
     const _pubkey = getPubkey()
     const _interest = 1000
     const _period = Math.floor(Date.now() / 1000) + 1200
-
-    const _sR = "f0f9862aeb53fb6bd587fa22d9e6705ca5c5c0ab2af67bba5042f2dc16d536e5"
-
-    const buf = new Buffer(_sR, 'hex');
-
-    const _rHash = bitcoin.crypto.sha256(buf)
-
-    console.log(_rHash.toString('hex'), pubkey)
+    const _sR = argSr
+    const buf = new Buffer(_sR, 'hex')
+    const rHash = bitcoin.crypto.sha256(buf)
+    const _rHash = '0x' + rHash.toString('hex')
 
     const submitOrder = await sw.submitOrder(
         _amountOfSat,
         _amountOfWei,
         _interest,
         _period,
-        '0x' + _rHash.toString('hex'),
+        _rHash,
         _pubkey, {
             value: 0,
             from: address
         })
 
-    console.log(submitOrder.logs)
+    console.log('Order ID: ', submitOrder.logs[0].args.orderId.toString())
     callback() // end process
 }
