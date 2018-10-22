f3ce9af00cc255cabc25fcb118ef5f3116796b28
Luca Ban - Black Birdy <mesqueeb@gmail.com>
2018-10-22 17:36:31 +0900

    greatly improve oracle price log 🎼

--- contract/txtest/ethtx/getPriceOracle.js
+++ contract/txtest/ethtx/getPriceOracle.js
@@ -1,13 +1,19 @@
-const {getAddress} = require('../utils/getAddress')
+const {getAddress} = require('../utils/address')
 
 const Swingby = artifacts.require('./Swingby.sol')
 const Token = artifacts.require('./Token.sol')
 
 const address = getAddress()
+const arg1 = Number(process.argv[4])
 
 module.exports = async function (callback) {
   const sw = await Swingby.deployed()
-  const price = await sw.getPrice()
-  console.log('The price of ETH/BTC  → ', price / 1e18)
+  const ethValueInBTC_ = await sw.getPrice()
+  const ethValueInBtc = ethValueInBTC_ / 1e18
+  console.log('1 BTC = ', 1 / ethValueInBtc , 'ETH')
+  console.log('1 ETH = ', 1 * ethValueInBtc, 'BTC')
+  if (!arg1 || arg1 === 1) return callback() // end process
+  console.log('================================')
+  console.log(`${arg1} BTC = `, arg1 / ethValueInBtc , 'ETH')
   callback() // end process
 }
